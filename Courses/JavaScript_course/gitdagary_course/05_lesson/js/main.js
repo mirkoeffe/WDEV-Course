a12f4580c59944eb988857b200d2baa5d9b14a40
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-14 03:42:58 +0100

    lesson n5-Code challenge

---
+++ Courses/JavaScript_course/gitdagary_course/05_lesson/js/main.js
@@ -0,0 +1,12 @@
+// My First Code Challenge
+
+// Write the code that will return a random letter from my name
+
+const anyName = "Mirko Effe"
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
