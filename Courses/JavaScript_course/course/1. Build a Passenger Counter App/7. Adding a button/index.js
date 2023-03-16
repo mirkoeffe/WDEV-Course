0e363ad0f6fbf6e8bd9449364fb3f277fe463a8b
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-16 21:46:33 +0100

    added the first button

--- Courses/JavaScript_course/course/1. Build a Passenger Counter App/7. Adding a button/index.js
+++ Courses/JavaScript_course/course/1. Build a Passenger Counter App/7. Adding a button/index.js
@@ -8,17 +8,19 @@
 // Decrease it down to 25, and then finally increase it to 70
 // Console.log the value after each step
 
-let bonusPoints = 50
-console.log(bonusPoints)
+let bonusPoints = 50;
+console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50;
+bonusPoints = bonusPoints * 2;
+console.log(bonusPoints);
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75;
+bonusPoints = bonusPoints / 4;
+console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
 
 
 
