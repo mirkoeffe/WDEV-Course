bcd5969dfecba05e7fb5f706cf52ce4e8ccabb15
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-17 00:54:53 +0100

    practicing with variables

--- Courses/JavaScript_course/course/2. Practice time - part 1/1. Variables practice/index.js
+++ Courses/JavaScript_course/course/2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,12 @@
 // Create two variables, firstName and lastName
 
+let firstName = "Mirko";
+let lastName = "Effe";
+
 // Concatenate the two variables into a third variable called fullName
+
+let fullName = firstName + " " + lastName;
 
 // Log fullName to the console
 
-
+console.log(fullName);
