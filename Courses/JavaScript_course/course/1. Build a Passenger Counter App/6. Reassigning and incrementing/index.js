0ed996be430c6afdb0cb4cee5bba4928061be8c5
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-16 21:43:33 +0100

    reassigning & incrementing

--- Courses/JavaScript_course/course/1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ Courses/JavaScript_course/course/1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -7,4 +7,17 @@ console.log(count)
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50;
+bonusPoints = bonusPoints * 2;
+console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+bonusPoints = bonusPoints / 4;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
 
