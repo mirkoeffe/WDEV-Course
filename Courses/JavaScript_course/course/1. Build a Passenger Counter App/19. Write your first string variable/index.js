1b906cfd4a69c87d59a001660ee51d415c5e972b
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-16 22:32:37 +0100

    first string variable written

--- Courses/JavaScript_course/course/1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ Courses/JavaScript_course/course/1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,8 @@
-let username = 'per"
+let username = "Mirko";
+let message = "You have three new notifications";
+let messageToUser = message + ", " + username + "!";
 
-console.log(username)
-
+console.log(username);
+console.log(message);
+console.log(username + ", " + message + "!");
+console.log(messageToUser);
