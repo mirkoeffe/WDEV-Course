89dc045b1fe80b2817901e1f18e73e4f23404f7e
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-16 22:15:22 +0100

    count is increasing at every click

--- Courses/JavaScript_course/course/1. Build a Passenger Counter App/14. Display the count/index.js
+++ Courses/JavaScript_course/course/1. Build a Passenger Counter App/14. Display the count/index.js
@@ -2,11 +2,14 @@
 
 // change the count-el in the HTML to reflect the new count
 
-let count = 0
+let countEL = document.getElementById("count-el");
+
+console.log(countEL);
+
+let count = 0;
 
 function increment() {
-    count = count + 1
-    console.log(count)
+  count = count + 1;
+  countEL.innerText = count;
+  console.log(count);
 }
-
-
