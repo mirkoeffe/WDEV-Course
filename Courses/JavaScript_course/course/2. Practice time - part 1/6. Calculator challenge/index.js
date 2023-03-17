1f257a3b3a1b637078514018d49d9404d41f2173
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-17 01:17:57 +0100

    practicing with functions and strings

--- Courses/JavaScript_course/course/2. Practice time - part 1/6. Calculator challenge/index.js
+++ Courses/JavaScript_course/course/2. Practice time - part 1/6. Calculator challenge/index.js
@@ -1,7 +1,7 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -11,4 +11,24 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
+let sumEl = document.getElementById("sum-el");
 
+function add() {
+  let result = num1 + num2;
+  sumEl.textContent = "Sum: " + result;
+}
+
+function subtract() {
+  let result = num1 - num2;
+  sumEl.textContent = "Sum: " + result;
+}
+
+function divide() {
+  let result = num1 / num2;
+  sumEl.textContent = "Sum: " + result;
+}
+
+function multiply() {
+  let result = num1 * num2;
+  sumEl.textContent = "Sum: " + result;
+}
