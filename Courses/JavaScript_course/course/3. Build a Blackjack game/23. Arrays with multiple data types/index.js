7c83f4d261f38a154a9dc3bcdd580ad3d8a59419
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-22 05:31:29 +0100

    arrays w/ multiple data types

--- Courses/JavaScript_course/course/3. Build a Blackjack game/23. Arrays with multiple data types/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/23. Arrays with multiple data types/index.js
@@ -2,3 +2,9 @@
 
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+let mirko = ["Mirko Fede", 31, true];
+console.log(mirko);
+
+let per = ["Per Harald Borgen", 35, true];
+console.log(per)
