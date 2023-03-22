800d7efc6300b9015905f594d38c936fd67a2113
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-22 03:32:55 +0100

    practicing boolean conditions

--- Courses/JavaScript_course/course/3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
@@ -1,10 +1,10 @@
-console.log(4 === 3)  // 
-console.log(5 > 2)    // 
-console.log(12 > 12)  //
-console.log(3 < 0)    //
-console.log(3 >= 3)   // 
-console.log(11 <= 11) //
-console.log(3 <= 2)   //
+console.log(4 === 3)  // false, 4 is not equal to 3
+console.log(5 > 2)    // true, 5 is greater than 2
+console.log(12 > 12)  // false, 12 isn't greater than 12
+console.log(3 < 0)    // false, 3 isn't less than 0
+console.log(3 >= 3)   // true, 3 isn't greater than 3 but is equal
+console.log(11 <= 11) // true, 11 isn't less than 11 but is equal
+console.log(3 <= 2)   // false, 3 isn't less than 2 or equal
 
 // let firstCard = 10
 // let secondCard = 11
@@ -23,3 +23,6 @@ console.log(3 <= 2)   //
 // }
 
 // console.log(isAlive)
+
+// less-than sign <
+// greater-than sign >
