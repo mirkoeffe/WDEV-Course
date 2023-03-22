a548fade74c12d0857a475705d45a20483620a19
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-22 02:04:02 +0100

    added if...else statements

--- Courses/JavaScript_course/course/3. Build a Blackjack game/3. If...else conditionals/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+const firstCard = 9;
+const secondCard = 11;
+const sum = firstCard + secondCard;
 
+if (sum < 21) {
+  console.log("Do you want a new card? 🤓");
+} else if (sum === 21) {
+  console.log("Jawhol!! You've got Blackjack! 🥳")
+} else {
+  console.log("You're out of the game! 😤")
+}
