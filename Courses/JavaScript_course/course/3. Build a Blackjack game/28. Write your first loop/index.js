7ee83e0831f2a4eeaf02121019669ade2c08f4b8
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-22 06:22:37 +0100

    for loops done

--- Courses/JavaScript_course/course/3. Build a Blackjack game/28. Write your first loop/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/28. Write your first loop/index.js
@@ -1,4 +1,10 @@
-
+for (let i = 0; i < 6; i += 1) {
+  console.log(i);
+}
 
 // Create a for loop that counts from 10 to 100 in steps of 10
 // Use console.log to log out the numbers
+
+for (let x = 10; x < 101; x += 10) {
+  console.log(x);
+}
--- Courses/JavaScript_course/course/3. Build a Blackjack game/29. For loops and arrays/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/29. For loops and arrays/index.js
@@ -1,10 +1,10 @@
 let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately.",
-    "Same here!",
-    "Great to hear"
-]
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on my portfolio lately.",
+  "Same here!",
+  "Great to hear",
+];
 
 // DRY - Don't repeat yourself
 // console.log(messages[0])
@@ -12,5 +12,13 @@ let messages = [
 // console.log(messages[2])
 // console.log(messages[3])
 
+// for (let i = 0; i < 5; i += 1) {
+//  console.log(i);
+//  console.log(messages[i]);
+// }
 
-
+// using the messages.length property the loop will always count up to the final index
+for (let i = 0; i < messages.length; i += 1) {
+    console.log(i); // it logs the numbers
+    console.log(messages[i]); // it logs the strings
+  }
