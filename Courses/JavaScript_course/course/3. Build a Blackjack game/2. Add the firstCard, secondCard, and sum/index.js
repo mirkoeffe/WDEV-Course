8f5856dc436041ce84cd463e0be4f83f559068a2
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-22 01:54:23 +0100

    1st 2 variables added

--- Courses/JavaScript_course/course/3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,11 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
+const firstCard = 9;
+const secondCard = 11;
+
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+const sum = firstCard + secondCard;
+
+console.log(sum);
