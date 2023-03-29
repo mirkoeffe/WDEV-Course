0c9f8a537d26b80cd1aab6034f17263fde2d87df
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-29 08:05:55 +0200

    objects

--- Courses/JavaScript_course/course/3. Build a Blackjack game/50. Intro to objects/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/50. Intro to objects/index.js
@@ -2,16 +2,25 @@
 // key-value pairs
 
 let course = {
-    title: "Learn CSS Grid for free",
-    lessons: 16,
-    creator: "Per Harald Borgen",
-    length: 63,
-    level: 2,
-    isFree: true,
-    tags: ["html", "css"]
-}
+  title: "Learn CSS Grid for free",
+  lessons: 16,
+  creator: "Per Harald Borgen",
+  length: 63,
+  level: 2,
+  isFree: true,
+  tags: ["html", "css"],
+};
 
+console.log(course.tags);
+console.log(course.isFree);
 
-console.log( course.tags )
+let party = {
+  title: "Klubnacht",
+  length: 350,
+  creator: "Dimitri Hegemann",
+  isFree: false,
+  tags: ["techno", "Music", "electronic"],
+};
 
-
+console.log(party.title);
+console.log(party.creator);
