d80f49918bbf721f8a2764f16ed6326034c5d505
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-29 08:12:41 +0200

    challenge: crate an object

--- Courses/JavaScript_course/course/3. Build a Blackjack game/51. Create your first object/index.js
+++ Courses/JavaScript_course/course/3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,16 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+  title: "2 beautiful rooms in a XVI century private castle",
+  price: 196,
+  rooms: 2,
+  isSuperHost: true,
+  images: ["img/castle1.png", "img/castle2.png"],
+  reviews: 52,
+  stars: 4.98,
+};
 
+console.log(castle.price);
+console.log(castle.stars);
+console.log(castle.isSuperHost);
