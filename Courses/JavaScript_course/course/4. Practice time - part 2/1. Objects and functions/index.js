d74c317dce7b4b7274753c4c964ca07628203786
mirkoeffe <mirkofede1991@yahoo.it>
2023-03-29 08:30:13 +0200

    1st challenge: done!

--- Courses/JavaScript_course/course/4. Practice time - part 2/1. Objects and functions/index.js
+++ Courses/JavaScript_course/course/4. Practice time - part 2/1. Objects and functions/index.js
@@ -2,7 +2,25 @@
 // Use yourself as an example to set the values for name, age, and country
 
 // Create a function, logData(), that uses the person object to create a
-// string in the following format: 
+// string in the following format:
 // "Per is 35 years old and lives in Norway"
 
 // Call the logData() function to verify that it works
+
+let person = {
+  name: "Mirko",
+  age: 31,
+  country: "Germany",
+};
+
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+
+logData();
