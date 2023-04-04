3f4ea154046faa7c2f608b8b3a90f4e003c289e8
mirkoeffe <mirkofede1991@yahoo.it>
2023-04-04 17:07:27 +0200

    day 33 #learnhowtocode

--- Courses/JavaScript_course/course/5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
+++ Courses/JavaScript_course/course/5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
@@ -1,9 +1,10 @@
 // template strings/literals
 
-const recipient = "James"
+const recipient = "James";
 // Create a new variable, sender, and set its value to your name
+const sender = "Mirko Fede";
 
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
 
-console.log(email)
+console.log(email);
