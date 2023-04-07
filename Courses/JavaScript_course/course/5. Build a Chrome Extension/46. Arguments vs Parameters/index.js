746a22d54ceaa3c5caaedb7a112d05537d1b67bb
mirkoeffe <mirkofede1991@yahoo.it>
2023-04-07 14:44:34 +0200

    functions with parameters/arguments inside

--- Courses/JavaScript_course/course/5. Build a Chrome Extension/46. Arguments vs Parameters/index.js
+++ Courses/JavaScript_course/course/5. Build a Chrome Extension/46. Arguments vs Parameters/index.js
@@ -1,7 +1,7 @@
-// What are greeting and name?
-// What are "Howdy" and "James"?
-// What are num1 and num2?
-// What are 3 and 4?
+// What are greeting and name?      parameters
+// What are "Howdy" and "James"?    arguments
+// What are num1 and num2?          parameters
+// What are 3 and 4?                arguments
 
 
 //.                parameters
@@ -17,5 +17,5 @@ greetUser(hi, "James")
 function add(num1, num2) {
     return num1 + num2
 }
-
+//. 
 add(3, 4)
