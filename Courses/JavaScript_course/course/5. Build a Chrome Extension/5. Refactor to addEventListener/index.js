0f10ba0cf007f4f9007cbc785279da9bd81f08ff
mirkoeffe <mirkofede1991@yahoo.it>
2023-04-07 16:47:06 +0200

    ...

--- Courses/JavaScript_course/course/5. Build a Chrome Extension/5. Refactor to addEventListener/index.js
+++
@@ -1,5 +0,0 @@
-let inputBtn = document.getElementById("input-btn");
-
-inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from addEventListener");
-});
--- Courses/JavaScript_course/course/5. Build a Chrome Extension/7. Your turn to refactor/index.js
+++
@@ -1,8 +0,0 @@
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
-
-let inputBtn = document.getElementById("input-btn");
-
-inputBtn.addEventListener("click", function () {
-  console.log("Button clicked!");
-});
