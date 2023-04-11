c9743a4dece0782613087f61ae6bf8c3cb1b19b7
mirkoeffe <mirkofede1991@yahoo.it>
2023-04-11 17:19:18 +0200

    #learnhowtocode day 37 done!

--- Courses/JavaScript_course/course/6. Practice time - part 3/6. Render images/index.js
+++ Courses/JavaScript_course/course/6. Practice time - part 3/6. Render images/index.js
@@ -2,8 +2,16 @@
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
 
-const imgs = [
-    "images/hip1.jpg",
-    "images/hip2.jpg",
-    "images/hip3.jpg"
-]
+const imgs = ["images/hip1.jpg", "images/hip2.jpg", "images/hip3.jpg"];
+
+const container = document.getElementById("container");
+
+function renderImages() {
+  let imgsDOM = "";
+  for (let i = 0; i < imgs.length; i++) {
+    imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`;
+  }
+  container.innerHTML = imgsDOM;
+}
+
+renderImages();
