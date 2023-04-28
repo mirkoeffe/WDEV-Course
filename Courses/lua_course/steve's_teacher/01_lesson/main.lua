70f423be739bac6bc11fe681929f94e0b99e4262
mirkoeffe <mirkofede1991@yahoo.it>
2023-04-28 03:08:50 +0200

    1st lesson

--- Courses/lua_course/steve's_teacher/01_lesson/main.lua
+++ Courses/lua_course/steve's_teacher/01_lesson/main.lua
@@ -1,1 +1,13 @@
-print("I'm cool")
+-- this is a comment
+-- and this is a comment too!
+
+--[[
+  this is a 
+  multiline comment
+  because of the brackets 
+]]
+
+print("Hello, I'm Mirko!")
+print("This is the beginning of my Lua Course")
+print("I'm adding the comma to the comment", "to see what happens!")
+print("I'm adding two dots between here " .. "and here to concatenate the two pieces of text")
